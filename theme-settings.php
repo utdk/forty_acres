7917891570af1cba9e1392b032f9b960080008a8
Mike Marsello <mmarsello@austin.utexas.edu>
2018-06-27 15:55:44 -0500

    add theme-settings.php

---
+++ theme-settings.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Theme settings which allow for configuration settings through the theme UI.
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements hook_form_FORM_ID_alter().
+ * @param $form
+ * @param \Drupal\Core\Form\FormStateInterface $form_state
+ */
+function forty_acres_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
+
+}
