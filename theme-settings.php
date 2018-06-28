dad7c08a83e0c44fb0d6358817440f13c7068840
Mike Marsello <mmarsello@austin.utexas.edu>
2018-06-28 15:10:12 -0500

    fix codesniffer warnings

--- theme-settings.php
+++ theme-settings.php
@@ -8,9 +8,7 @@
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Implements hook_form_FORM_ID_alter().
- * @param $form
- * @param \Drupal\Core\Form\FormStateInterface $form_state
+ * Implements hook_form_system_theme_settings_alter().
  */
 function forty_acres_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
 
