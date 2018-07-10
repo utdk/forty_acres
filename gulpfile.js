747f8b4a5891bcf456e01687cc554fcae999a719
Michael J Marsello <mmarsello@austin.utexas.edu>
2018-07-10 12:15:56 -0500

    UDK8 416: UT brand fonts (#10)
    * add font files
    
    * add gulp task to compile fonts scss and add library
    
    * add newline and update gulp clean task
    
    * compile scss into /css
    
    * remove dist from gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,15 @@ var uglify = require("gulp-uglify");
 var del = require("del");
 var sequence = require("gulp-sequence");
 var autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("sass", function () {
+  return gulp.src("./src/scss/build/**/*.scss")
+    .pipe(sass({outputStyle: 'expanded'}).on("error", sass.logError))
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions"],
+      cascade: false
+    }))
+    .pipe(gulp.dest("css"));
+});
+
+gulp.task("default", sequence("sass"));
