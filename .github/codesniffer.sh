8e747c6e660d24700164848d29b677e56d97c4b2
Michael J Marsello <mmarsello@austin.utexas.edu>
2018-07-13 10:37:31 -0500

    add .theme extension to list of codesniffed file types (#15)

--- .github/codesniffer.sh
+++ .github/codesniffer.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# The following file must exist & must define the 
+# The following file must exist & must define the
 # PHPCS_BIN and RULESET variables.
 # See example.config.sh
 source "${BASH_SOURCE[0]%/*}/config.sh"
 
-PHP_LIST=$( git diff --name-only --cached --diff-filter=ACM -- '*.php' '*.inc' '*.yml' '*.module' '*.install')
-echo 
+PHP_LIST=$( git diff --name-only --cached --diff-filter=ACM -- '*.php' '*.inc' '*.yml' '*.module' '*.install' '*.theme')
+echo
 if [ ! -f $PHPCS_BIN ];
   then
     echo "WARNING: PHP Code Sniffer was not found. This commit might include coding standards violations. You can fix this by ensuring that composer has correctly installed it's dev dependencies including a symlink in the bin folder to the phpcs package in vendor."
