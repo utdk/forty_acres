00385f77c4b6277ac9da05f41ee10428170a232c
Michael J Marsello <mmarsello@austin.utexas.edu>
2018-07-16 12:31:24 -0500

    UDK8 476: Update CSS/SCSS linting config (#16)
    * add gulp-stylelint
    
    * disable no-browser-hacks in custom.scss
    
    * add gulp-csscombx
    
    * add .csscombx.json config file
    
    * update csscomb ordering rules
    
    * csscomb rule cleanup
    
    * reorder vendor prefix rules
    
    * remove stylelint check from pre-commit hook
    
    * only lint CSS from /src and copy those files to /css

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,45 @@ var uglify = require("gulp-uglify");
 var del = require("del");
 var sequence = require("gulp-sequence");
 var autoprefixer = require("gulp-autoprefixer");
+var gulpStylelint = require("gulp-stylelint");
+var csscombx = require("gulp-csscombx");
+
+gulp.task("scss-lint", function lintCssTask() {
+  return gulp
+    .src("src/scss/**/*.scss")
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: "string", console: true}
+      ],
+      failAfterError: false
+    }));
+});
 
 gulp.task("sass", function () {
   return gulp.src("./src/scss/build/**/*.scss")
-    .pipe(sass({outputStyle: 'expanded'}).on("error", sass.logError))
+    .pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
     .pipe(autoprefixer({
       browsers: ["last 2 versions"],
       cascade: false
     }))
+    .pipe(csscombx())
     .pipe(gulp.dest("css"));
 });
 
-gulp.task("default", sequence("sass"));
+gulp.task("copy-css", function(){
+  return gulp.src("src/css/**/*.css")
+  .pipe(gulp.dest("css"));
+});
+
+gulp.task("css-lint", function lintCssTask() {
+  return gulp
+    .src("src/css/**/*.css")
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: "string", console: true}
+      ],
+      failAfterError: false
+    }));
+});
+
+gulp.task("default", sequence("scss-lint", "sass", "css-lint", "copy-css"));
