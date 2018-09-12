20279843112df89fbe680b5cb983ab46b2678cf3
Michael J Marsello <mmarsello@austin.utexas.edu>
2018-09-12 11:18:41 -0500

    UDK8-515: theming the main menu (#34)
    * add menu templates and main-menu scss
    
    * add js for subnav trigger
    
    * update container max-width with values from TDS
    
    * add sub-nav-item class to L2 list-items
    
    * make some class names more bem-like
    
    * fix linting notices
    
    * fix linting violations
    
    * fix linting violations
    
    * fix linting violations
    
    * update styling of header menu
    
    * add margin between header regions
    
    * fix menu visibility at 900px
    
    * relative position on #ut-main_menu-wrapper
    
    * edited the main-nav.scss to add flexbox styles so each menu item is same width no matter how long the link text is
    
    * add dynamic class to primary menu region
    
    * undo flexbox changes to main nav as it actually looks better the way it was
    
    * fix collapsing div
    
    * fix spacing on mobile header
    
    * spacing elements and remove logic from .theme
    
    * flexify regions so items align right
    
    * fix layout bug at 900px

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+// /**
+//  * @file
+//  * Placeholder file for custom sub-theme behaviors.
+//  *
+//  */
+(function ($, Drupal) {
+
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  Drupal.behaviors.headerBehavior = {
+    attach: function (context, settings) {
+
+      var menuToggler = $('#js-nav-toggle');
+      var togglerTarget = $('#ut-main_menu-wrapper');
+      menuToggler.on('click', function () {
+        togglerTarget.toggleClass('active');
+        togglerTarget.hasClass('active') ? menuToggler.html('CLOSE') : menuToggler.html('MENU');
+      });
+
+    }
+  };
+
+})(jQuery, Drupal);
