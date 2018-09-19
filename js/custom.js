b2b6d7c347943e6dac2b98fae8bf42283c77aade
Michael J Marsello <mmarsello@austin.utexas.edu>
2018-09-19 13:07:32 -0500

    UDK8-516: Initialize megamenu library on the main menu (#39)
    * edit footer.html.twig to put in conditional to change classes onregion to layout works with  only first and last regions
    
    * move nav tag back to block template
    
    * get aria attributes working
    
    * comment out icon related scss and update amm settings object

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 // /**
 //  * @file
-//  * Placeholder file for custom sub-theme behaviors.
+//  * Placeholder file for custom theme behaviors.
 //  *
 //  */
 (function ($, Drupal) {
@@ -11,12 +11,27 @@
   Drupal.behaviors.headerBehavior = {
     attach: function (context, settings) {
 
+      // Enable main menu dropdown trigger.
       var menuToggler = $('#js-nav-toggle');
       var togglerTarget = $('#ut-main_menu-wrapper');
       menuToggler.on('click', function () {
         togglerTarget.toggleClass('active');
         togglerTarget.hasClass('active') ? menuToggler.html('CLOSE') : menuToggler.html('MENU');
       });
+
+      // Initialize megamenu library on main menu.
+      $('nav#block-main-navigation').accessibleMegaMenu({
+          uuidPrefix: "accessible-megamenu",
+          menuClass: "nav-menu",
+          topNavItemClass: "main-menu__list-item",
+          panelClass: "sub-nav",
+          panelGroupClass: "sub-nav-wrapper",
+          hoverClass: "hover",
+          focusClass: "focus",
+          openClass: "open",
+          openDelay: 100,
+          closeDelay: 100
+		  });
 
     }
   };
