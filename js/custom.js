f93d20da4f4bee473269f39fd5827a185c8eded4
Michael J Marsello <mmarsello@austin.utexas.edu>
2018-09-20 12:24:56 -0500

    UDK8-523: Styles desktop dropdowns (#41)
    * edit footer.html.twig to put in conditional to change classes onregion to layout works with  only first and last regions
    
    * move nav tag back to block template
    
    * get aria attributes working
    
    * comment out icon related scss and update amm settings object
    
    * add caret icon and add new class for menu links
    
    * style desktop dropdowns
    
    * add overflowing and column-classes to subnavs
    
    * fix column break issues on subnavs
    
    * add icons
    
    * set overflowing class on another element
    
    * commnet out animation related scss
    
    * remove duplicate scss
    
    * set up mobile dropdown trigger
    
    * CSS cleanup and improve comments
    
    * cleanup js comments

--- js/custom.js
+++ js/custom.js
@@ -33,6 +33,38 @@
           closeDelay: 100
 		  });
 
+      // Adding columns to the navigation dynamically.
+      $('nav#block-main-navigation .sub-nav').each(function(i, el){
+        var $el = $(el),
+            $main = $('.main-menu__list'),
+            children = $el.find('.main-menu__list-item--sub-menu'),
+            containerWidth;
+          // Get the max row width.
+          containerWidth = $el.parent().parent().width();
+          // Still in Mobile View.
+          if (!containerWidth) { return; }
+          // Add column classes based on number of submenu items.
+          var numColumns = (children.length % 3 === 0) ? 'three-columns' : 'two-columns';
+          if (children.length === 1) {
+              numColumns = 'one-column';
+          }
+          $el.addClass(numColumns);
+          // Insure subnavs don't open to the right of the viewport.
+          var numChildren = $main.children();
+          numChildren.each(function(e) {
+            if (e >= (numChildren.length / 2)) {
+              $(this).find('.sub-nav').addClass('overflowing');
+            }
+          })
+      });
+
+      // Make click event on L2 links on mobile menu trigger.
+      $('.icon.subnav-trigger').on('click', function() {
+        $(this).toggleClass('icon-open');
+        $(this).siblings('.sub-nav-wrapper').find('.main-menu__list--sub-menu').toggleClass('open');
+        $(this).parent('.main-menu__list-item').siblings().find('.main-menu__list--sub-menu').removeClass('open');
+      });
+
     }
   };
 
