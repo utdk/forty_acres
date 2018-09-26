72bf3fab91e8fe9a65a991510cd6ddc1068eec8f
Michael J Marsello <mmarsello@austin.utexas.edu>
2018-09-26 16:08:02 -0500

    UDK8 524: mobile nav styling and functionality/accessibility (#44)
    * rename --sub-menu modifier to --subnav
    
    * add touchstart event for triggers on iphones
    
    * set aria-expanded on main-nav trigger
    
    * add aria attributes for mobile nav
    
    * update z-index, make mobile L1 clickable
    
    * improve keyboard navigability on mobile
    
    * update attributes on main menu button
    
    * vertically stack header menu on xs screens
    
    * disable mouseover event on mobile
    
    * fix display bugs at 900px
    
    * remove icon click event on desktop and wrap resize event in debounce
    
    * reset defaults on resize
    
    * leave mouseover event on on mobile

--- js/custom.js
+++ js/custom.js
@@ -3,21 +3,13 @@
 //  * Placeholder file for custom theme behaviors.
 //  *
 //  */
-(function ($, Drupal) {
+(function ($, Drupal, debounce) {
 
   /**
    * Use this behavior as a template for custom Javascript.
    */
   Drupal.behaviors.headerBehavior = {
     attach: function (context, settings) {
-
-      // Enable main menu dropdown trigger.
-      var menuToggler = $('#js-nav-toggle');
-      var togglerTarget = $('#ut-main_menu-wrapper');
-      menuToggler.on('click', function () {
-        togglerTarget.toggleClass('active');
-        togglerTarget.hasClass('active') ? menuToggler.html('CLOSE') : menuToggler.html('MENU');
-      });
 
       // Initialize megamenu library on main menu.
       $('nav#block-main-navigation').accessibleMegaMenu({
@@ -31,13 +23,24 @@
           openClass: "open",
           openDelay: 100,
           closeDelay: 100
-		  });
+      });
+
+      // Enable main menu dropdown trigger and set aria-expanded attribute.
+      var menuToggler = $('#js-nav-toggle');
+      var togglerTarget = $('#ut-main_menu-wrapper');
+      menuToggler.on('click', function () {
+        togglerTarget.toggleClass('active');
+        togglerTarget.hasClass('active') ? menuToggler.html('CLOSE') : menuToggler.html('MENU');
+        menuToggler.attr('aria-expanded', function(index, attr) {
+          return (attr == 'true') ? 'false' : 'true';
+        });
+      });
 
       // Adding columns to the navigation dynamically.
       $('nav#block-main-navigation .sub-nav').each(function(i, el){
         var $el = $(el),
             $main = $('.main-menu__list'),
-            children = $el.find('.main-menu__list-item--sub-menu'),
+            children = $el.find('.main-menu__list-item--subnav'),
             containerWidth;
           // Get the max row width.
           containerWidth = $el.parent().parent().width();
@@ -59,13 +62,59 @@
       });
 
       // Make click event on L2 links on mobile menu trigger.
-      $('.icon.subnav-trigger').on('click', function() {
-        $(this).toggleClass('icon-open');
-        $(this).siblings('.sub-nav-wrapper').find('.main-menu__list--sub-menu').toggleClass('open');
-        $(this).parent('.main-menu__list-item').siblings().find('.main-menu__list--sub-menu').removeClass('open');
+      var iconClick = function() {
+        $('icon.subnav-trigger').off('click').on('click', function() {
+          $(this).toggleClass('icon--open');
+          // Add 'open' class to subnav associated with icon.
+          $(this).siblings('.sub-nav-wrapper').find('.main-menu__list--subnav').toggleClass('open');
+          // Remove 'open' class from all other subnavs.
+          $(this).parent('.main-menu__list-item').siblings().find('.main-menu__list--subnav').removeClass('open');
+          // Toggle aria attributes.
+          $(this).siblings('.main-menu__link').attr('aria-expanded', function(index, attr) {
+            return (attr == 'true') ? 'false' : 'true';
+          });
+          $(this).siblings('.sub-nav-wrapper').attr('aria-hidden', function(index, attr) {
+            return (attr == 'true') ? 'false' : 'true';
+          });
+          $(this).siblings('.sub-nav-wrapper').attr('aria-expanded', function(index, attr) {
+            return (attr == 'true') ? 'false' : 'true';
+          });
+          // Reset aria attributes of the clicked item's siblings.
+          $(this).parent('.main-menu__list-item').siblings().find('.sub-nav-wrapper').attr('aria-expanded', 'false').attr('aria-hidden', 'true');
+          $(this).parent('.main-menu__list-item').siblings().find('.main-menu__link').attr('aria-expanded', 'false');
+        });
+      }
+
+      // Reset aria attributes and remove dynamic classes from main menu list items.
+      var resetDefaults = function() {
+        $('ul.main-menu__list.nav-menu .sub-nav-wrapper').removeClass('open hover focus').attr('aria-expanded', 'false').attr('aria-hidden', 'true');
+        $('ul.main-menu__list.nav-menu .main-menu__link').removeClass('open').attr('aria-expanded', 'false');
+        $('ul.main-menu__list.nav-menu .main-menu__list--subnav').removeClass('open');
+        $('#js-nav-toggle').html('MENU').attr('aria-expanded', 'false');
+        $('#ut-main_menu-wrapper').removeClass('active');
+        $('icon.subnav-trigger').removeClass('icon--open');
+      }
+
+      // Undo preventDefault called on L1 links so they are clickable.
+      $('.menu-item-has-children > a').on('touchstart', function(){
+        window.location.href = $(this).attr('href');
       });
+
+      var resizeEvent = debounce(function() {
+        // Reset to defaults
+        resetDefaults();
+        // Disable mouseover events and attach click event on mobile.
+        if (window.innerWidth < 900) {
+          iconClick();
+        }
+        // Disable click event on chevron on desktop.
+        else {
+          $('icon.subnav-trigger').off('click');
+        }
+      }, 100);
+      $(window).on('load resize', resizeEvent);
 
     }
   };
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, Drupal.debounce);
