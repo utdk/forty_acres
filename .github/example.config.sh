aac78de48c0937a7d4ec0aaefab43d250a6ab497
jmf3658 <jfullmer@austin.utexas.edu>
2024-05-30 14:16:13 -0500

    Redact information about Environment Generator

--- .github/example.config.sh
+++
@@ -1,4 +0,0 @@
-# The path to the phpcs executable.
-PHPCS_BIN=~/.composer/vendor/bin/phpcs
-# The path to the Drupal PHPCS ruleset.
-RULESET=~/.composer/vendor/drupal/coder/coder_sniffer/Drupal/ruleset.xml
