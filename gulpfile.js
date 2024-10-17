e20fde426d818fec946315d3b311ac695a939842
jmf3658 <jfullmer@austin.utexas.edu>
2024-10-17 17:07:38 -0500

    Bootstrap 5

--- gulpfile.js
+++ gulpfile.js
@@ -28,4 +28,20 @@ gulp.task("sass", function () {
     .pipe(gulp.dest("css"));
 });
 
-gulp.task("default", gulp.series("scss-lint", "sass"));
+gulp.task('copy-js', function () {
+  return gulp
+    .src([
+      './node_modules/accessible-mega-menu/js/jquery-accessibleMegaMenu.js',
+      './node_modules/bootstrap/js/dist/tab.js',
+      './node_modules/bootstrap/js/dist/util.js',
+      './node_modules/bootstrap/js/dist/collapse.js',
+      './node_modules/bootstrap/js/dist/alert.js',
+      './node_modules/bootstrap/js/dist/modal.js',
+      './node_modules/bootstrap/js/dist/tooltip.js',
+      './node_modules/popper.js/dist/umd/popper.js'
+    ])
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("default", gulp.series("scss-lint", "sass", "copy-js"));
